dd751a25feb9a88f0c8f1e524262bb400e40a18a
jolive1993 <rockitshipshawty@gmail.com>
2016-03-24 13:58:08 -0500

    dropbox

---
+++ Outa/App_Start/DropBoxConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Dropbox.Api;
+
+
+namespace Outa
+{
+    public class DropBoxConfig
+    {
+        public static void Init()
+        {
+            var task = Task.Run((Func<Task>)DropBoxConfig.Run);
+            task.Wait();
+        }
+
+         private static async Task Run()
+        {
+            using (var dbx = new DropboxClient("YOUR ACCESS TOKEN"))
+            {
+                var full = await dbx.Users.GetCurrentAccountAsync();
+                Console.WriteLine("{0} - {1}", full.Name.DisplayName, full.Email);
+            }
+        }
+    }
+}
