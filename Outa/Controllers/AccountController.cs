95c5d59ff6211eaac803739f360a6d7656653cf7
jolive1993 <rockitshipshawty@gmail.com>
2016-03-26 17:56:36 -0500

    changed navbar

--- Outa/Controllers/AccountController.cs
+++ Outa/Controllers/AccountController.cs
@@ -387,8 +387,6 @@ namespace Outa.Controllers
 
         //
         // POST: /Account/LogOff
-        [HttpPost]
-        [ValidateAntiForgeryToken]
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
